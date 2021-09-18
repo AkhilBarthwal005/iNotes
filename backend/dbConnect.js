b9cadbea1351710f32a013deea76c65207af8a60
AkhilBarthwal005 <akhil.barthwal005@gmail.com>
2021-09-18 15:11:12 +0530

    Connection made to databse

---
+++ backend/dbConnect.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const mongoURL =
+  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+
+const connectToMongo = () => {
+  mongoose.connect(mongoURL, () => {
+    console.log("Connect to Mongo successfully.");
+  });
+};
+
+module.exports = connectToMongo;
